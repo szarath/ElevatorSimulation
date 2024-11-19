b2657fb31029a7fd3718df95b08c3ec0f0447010
Szarath Kumar <szarath@users.noreply.github.com>
2024-11-19 20:57:55 +0200

    Added Constants class with max passenger and weight limit

---
+++ ElevatorSimulation/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorSimulation
+{
+    public static class Constants
+    {
+        public const int MaxPassengers = 10;
+        public const int WeightLimit = 1500; // In kilograms
+    }
+}
