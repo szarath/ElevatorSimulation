9d58bf7bdd1d64bcd0694c46190064245bcf53b5
Szarath Kumar <szarath@users.noreply.github.com>
2024-11-19 21:09:52 +0200

    Created IElevator interface with core elevator operations

---
+++ ElevatorSimulation/IElevator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorSimulation
+{
+    public interface IElevator
+    {
+        int CurrentFloor { get; set; }
+        ElevatorDirection Direction { get; set; }
+        int PassengersCount { get; set; }
+        int WeightLimit { get; set; }
+
+        void Move(int targetFloor);
+        void DisplayStatus();
+        void PickupPassengers(int passengerCount);
+        void DropOffPassengers(int passengerCount);
+        bool CanCarryPassengers(int passengerCount);
+        void DisplayElevatorDetails();
+    }
+
+}
